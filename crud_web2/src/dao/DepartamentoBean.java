d88736235984f65bcffe4c01951ad42a453d5013
Rodolpho Erick <rodolphoerick90@gmail.com>
2019-03-27 16:00:15 -0300

    Pacote bean transformado em pacote DAO para seguir os requisitos da atividade.

--- crud_web2/src/dao/DepartamentoBean.java
+++ crud_web2/src/dao/DepartamentoBean.java
@@ -1,4 +1,4 @@
-package bean;
+package dao;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
--- crud_web2/src/dao/DepartamentoDAO.java
+++
@@ -1,5 +0,0 @@
-package dao;
-
-public class DepartamentoDAO {
-	//O MBean chama a classe de negócio que chama a classe DAO. As classes que estão no PDF ficarão no modelo.
-}
